b50ade77ace9223ff7d0a3dbc9762f4870fa936e
amoled27 <amodshinde1995@gmail.com>
2017-05-11 17:38:34 +0530

    dish displayed

--- src/app/menu-overview/menu-group-dish-detail/menu-group-dish-detail.component.ts
+++ src/app/menu-overview/menu-group-dish-detail/menu-group-dish-detail.component.ts
@@ -21,13 +21,18 @@ export class MenuGroupDishDetailComponent implements OnInit {
 
   ngOnInit(){
     //https://stackoverflow.com/questions/34906888/angular-2-access-parent-routeparams-from-child-component
-    this.subscription = this.route.parent.params.subscribe(params => {
-      this.menuGroupItemID = +params["menuGroupItemID"];
-      console.log("Received Parent menuGroupID with id : " + this.menuGroupItemID + " in MenuGroupDishesComponent")
-    });
+    // this.subscription = this.route.parent.params.subscribe(params => {
+    //   this.menuGroupItemID = +params["menuGroupItemID"];
+    //   console.log("Received Parent menuGroupItemID with id : " + this.menuGroupItemID + " in MenuGroupDisheDetailComponent")
+    // });
+     this.subscription = this.route.params.subscribe(
+      (param: any) => {
+        this.menuGroupItemID = param['DishID'];
+        console.log("Accepted Menu Group ID Params with id : " + this.menuGroupItemID);
+      });
 
     this.getDishDetail();
-
+ 
 
   }
 
@@ -35,7 +40,7 @@ export class MenuGroupDishDetailComponent implements OnInit {
     this._menuService.getMenuItem(this.menuGroupItemID).subscribe(
       menuGroupItem => {
         this.menuGroupItem = menuGroupItem;
-        console.log(menuGroupItem);
+        console.log("ulala"+ menuGroupItem);
       },
       err => {
         // Log errors if any
